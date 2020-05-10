a381a09086892f6e5505e2a68b8d16d2dfc66b59
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-05-10 13:41:23 +0200

    Company generated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { User } from './User';
+import { Company } from './Company';
 
 // Create instace of User
 const user = new User();
 console.log(user);
+
+// Create instace of Company
+const company = new Company();
+console.log(company);
